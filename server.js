0a26188774cc15a6684844a4328641fdfecc15fb
kubi2392 <121381391+kubi2392@users.noreply.github.com>
2025-01-23 16:19:31 +0100

    header update

--- server.js
+++ server.js
@@ -2,22 +2,15 @@ var express = require('express');
 var app = express();
 
 // set the view engine to ejs
+//"type": "commonjs",
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-      ];
-      var tagline = "No programming concept is complete without a cute animal mascot.";
-  res.render('pages/index', {
-    mascots: mascots,
-    tagline: tagline
-  });
+      
+  res.render('pages/index');
 });
 
 // about page
