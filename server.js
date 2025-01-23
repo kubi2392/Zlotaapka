cb38f3b41bcc5282c461e221a97a5ec6e3b5292c
kubi2392 <121381391+kubi2392@users.noreply.github.com>
2025-01-23 23:41:53 +0100

    Template of login and register

--- server.js
+++ server.js
@@ -2,21 +2,26 @@ var express = require('express');
 var app = express();
 
 // set the view engine to ejs
-//"type": "commonjs",
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
       
-  res.render('pages/index');
+  res.render('pages/public/index');
 });
 
 // about page
 app.get('/about', function(req, res) {
-  res.render('pages/about');
+  res.render('pages/public/about');
 });
-
+// login page
+app.get('/login', function(req, res) {
+  res.render('pages/public/login');
+});
+// register page
+app.get('/register', function(req, res) {
+  res.render('pages/public/register');
+});
 app.listen(8080);
 console.log('Server is listening on port 8080');
